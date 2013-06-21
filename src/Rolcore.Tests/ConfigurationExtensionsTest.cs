371b88763e5e2c50033b896d5cbeb83fbb93690a
James Tharpe <jimmy.tharpe@gmail.com>
2013-06-21 10:48:58 -0400

    Adds ConfigurationExtensions to make reading and deleting app settings more convenient.

---
+++ src/Rolcore.Tests/ConfigurationExtensionsTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rolcore.Configuration;
+using System.Configuration;
+
+namespace Rolcore.Tests
+{
+    [TestClass]
+    public class ConfigurationExtensionsTest
+    {
+        [TestMethod]
+        public void WriteAppSetting_AddsNewSetting()
+        {
+            var target = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var setting = target.AppSettings.Settings["temp setting"];
+            Assert.IsNull(setting, "setting already exists");
+
+            target.WriteAppSetting("temp setting", "arbitrary value");
+            setting = target.AppSettings.Settings["temp setting"];
+            Assert.AreEqual("arbitrary value", setting.Value);
+
+            target.DeleteAppSetting("temp setting");
+        }
+
+        [TestMethod]
+        public void DeleteAppSetting_DeletesSetting()
+        {
+            var target = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            target.WriteAppSetting("temp setting", "arbitrary value");
+
+            target.DeleteAppSetting("temp setting");
+            var setting = target.AppSettings.Settings["temp setting"];
+            Assert.IsNull(setting, "setting not deleted");
+        }
+
+        [TestMethod]
+        public void WriteAppSetting_UpdatesExistingSetting()
+        {
+            var target = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var setting = target.AppSettings.Settings["existing setting"];
+            var settingValue = DateTime.UtcNow.ToString("u");
+            Assert.AreNotEqual(settingValue, setting.Value);
+
+            target.WriteAppSetting("existing setting", settingValue);
+            Assert.AreEqual(settingValue, setting.Value); 
+        }
+    }
+}
---
+++ src/Rolcore/Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace Rolcore.Configuration
+{
+    /// <summary>
+    /// Extension methods for <see cref="System.Configuration.Configuration"/>.
+    /// </summary>
+    public static class ConfigurationExtensions
+    {
+        public static void WriteAppSetting(this System.Configuration.Configuration config, string key, string value)
+        {
+            Contract.Requires<ArgumentNullException>(config != null, "config is null.");
+            Contract.Requires<InvalidOperationException>(config.AppSettings != null, "config does not contain AppSettings.");
+            Contract.Requires<InvalidOperationException>(!config.AppSettings.IsReadOnly(), "AppSettings is read-only.");
+            Contract.Requires<InvalidOperationException>(!config.AppSettings.SectionInformation.IsLocked, "AppSettings section is locked.");
+            Contract.Requires<ArgumentException>(!String.IsNullOrEmpty(key), "key is null or empty.");
+            Contract.Requires<ArgumentException>(!String.IsNullOrEmpty(value), "value is null or empty.");
+
+            if (config.AppSettings.Settings.AllKeys.Contains(key))
+            {
+                config.AppSettings.Settings[key].Value = value;
+            }
+            else
+            {
+                config.AppSettings.Settings.Add(new KeyValueConfigurationElement(key, value));
+            }
+    
+            config.Save();
+        }
+
+        public static void DeleteAppSetting(this System.Configuration.Configuration config, string key)
+        {
+            Contract.Requires<ArgumentNullException>(config != null, "config is null.");
+            Contract.Requires<InvalidOperationException>(config.AppSettings != null, "config does not contain AppSettings.");
+            Contract.Requires<InvalidOperationException>(!config.AppSettings.IsReadOnly(), "AppSettings is read-only.");
+            Contract.Requires<InvalidOperationException>(!config.AppSettings.SectionInformation.IsLocked, "AppSettings section is locked.");
+            Contract.Requires<ArgumentException>(!String.IsNullOrEmpty(key), "key is null or empty.");
+
+            config.AppSettings.Settings.Remove(key);
+            config.Save();
+        }
+    }
+}
