845b7aef29624bb1b5b694955ed4f65c0e65cf5b
Daniel Gelbaum <danielgelbaum@gmail.com>
2013-08-07 13:00:21 -0400

    new mock rule for the detail table

---
+++ src/Rolcore.Repository.Tests/Mocks/MockRepositoryItemDetailRule.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace Rolcore.Repository.Tests.Mocks
+{
+    public class MockRepositoryItemDetailRule<TItem> : IRepositoryItemRule<TItem>
+        where TItem : class
+    {
+        public MockRepositoryItemDetailRule()
+        {
+            ApplyWasCalled = false;
+        }
+
+        public void Apply(TItem item)
+        {
+            Debug.WriteLine(
+                string.Format("Rule {0} applied to detail item {1}", this, item));
+            ApplyWasCalled = true;
+        }
+
+        public bool ApplyWasCalled
+        {
+            get;
+            private set;
+        }
+    }
+}
